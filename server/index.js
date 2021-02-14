ee7b03b348381fcefc6dfff2938f4fc6199def44
jatinudasi <jatin.udasi88@gmail.com>
2021-02-14 20:52:21 +0530

    added morgan

--- server/index.js
+++ server/index.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 require('dotenv').config();
 require('./database/mongodbinit');
+
+
 
 
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(morgan('dev'));
 
 const allroutes = require('./routes/index');
 
